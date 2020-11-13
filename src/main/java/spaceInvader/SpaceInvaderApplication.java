2e462650b143098cad9650551cbf67a7196c1edd
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-13 14:28:34 +0200

    Smoother rotation

--- src/main/java/spaceInvader/SpaceInvaderApplication.java
+++ src/main/java/spaceInvader/SpaceInvaderApplication.java
@@ -1,5 +1,6 @@
 package spaceInvader;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -7,6 +8,9 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpaceInvaderApplication extends Application {
 
@@ -22,17 +26,24 @@ public class SpaceInvaderApplication extends Application {
 
 		screen.getChildren().add(spaceship);
 
+		Map<KeyCode, Boolean> keyPressed = new HashMap<>();
+
 		Scene scene = new Scene(screen);
+		scene.setOnKeyPressed(event -> keyPressed.put(event.getCode(), true));
+		scene.setOnKeyReleased(event -> keyPressed.put(event.getCode(), false));
 
-		scene.setOnKeyPressed(event -> {
-			if (event.getCode().equals(KeyCode.LEFT)) {
-				spaceship.setRotate(spaceship.getRotate() - 5);
-			}
+		new AnimationTimer() {
+			@Override
+			public void handle(long present) {
+				if (keyPressed.getOrDefault(KeyCode.LEFT, false)) {
+					spaceship.setRotate(spaceship.getRotate() - 5);
+				}
 
-			if (event.getCode().equals(KeyCode.RIGHT)) {
-				spaceship.setRotate(spaceship.getRotate() + 5);
+				if (keyPressed.getOrDefault(KeyCode.RIGHT, false)) {
+					spaceship.setRotate(spaceship.getRotate() + 5);
+				}
 			}
-		});
+		}.start();
 
 		stage.setTitle("<< SPACE INVADER >>");
 		stage.setScene(scene);
