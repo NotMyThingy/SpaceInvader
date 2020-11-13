edf32b8291fe2ed9b28b8374c76297e250d13a3e
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-13 15:52:13 +0200

    refactored to inherit abstract class, Sprite

--- src/main/java/spaceInvader/Spaceship.java
+++ src/main/java/spaceInvader/Spaceship.java
@@ -3,40 +3,16 @@ package spaceInvader;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
-public class Spaceship {
+public class Spaceship extends Sprite {
 
-	private final Polygon ship;
 	private Point2D movement;
 
 	public Spaceship(int x, int y) {
-		ship = new Polygon(-5, -5, 10, 0, -5, 5);
-		ship.setTranslateX(x);
-		ship.setTranslateY(y);
-
-		movement = new Point2D(0, 0);
+		super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
 	}
 
 	public Polygon getShip() {
-		return ship;
+		return super.getSprite();
 	}
 
-	public void turnLeft() {
-		ship.setRotate(ship.getRotate() - 5);
-	}
-
-	public void turnRight() {
-		ship.setRotate(ship.getRotate() + 5);
-	}
-
-	public void move() {
-		ship.setTranslateX(ship.getTranslateX() + movement.getX());
-		ship.setTranslateY(ship.getTranslateY() + movement.getY());
-	}
-
-	public void accelerate() {
-		double accelX = Math.cos(Math.toRadians(ship.getRotate()));
-		double accelY = Math.cos(Math.toRadians(ship.getRotate()));
-
-		movement = movement.add(accelX, accelY);
-	}
 }
