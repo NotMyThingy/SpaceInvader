f43b1f4e20f6bf0dcd5f593aced7d7685fc12316
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-13 14:05:12 +0200

    main

---
+++ src/main/java/spaceInvader/Main.java
@@ -0,0 +1,10 @@
+package spaceInvader;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(SpaceInvaderApplication.class);
+	}
+}
